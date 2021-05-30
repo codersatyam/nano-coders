68532c49f2dc723dc12b93d31f4ac1631ff2e3aa
Your Name <you@example.com>
2021-05-30 11:59:13 +0530

    changed color

--- buy/urls.py
+++ buy/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path("", views.buyhome, name="buyhome")
+    path("", views.buyhome, name="buyhome"),
+    path("sensor", views.sensor, name="sensor")
     ]
--- buy/views.py
+++ buy/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def buyhome(request):
     return render(request, "buy/buyhome.html")
+def sensor(request):
+    return render(request,"buy/sensor.html")
